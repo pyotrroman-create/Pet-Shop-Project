a1a49626de73436a9c28f21dc73fb4f0865c2c63
Petro <Petro.Romanenko@magenta.de>
2026-09-25 19:29:32 +0200

    Responsive header

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,10 +1,13 @@
 import { NavLink, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import styles from "./Header.module.css";
 import logo from "../assets/icons/Logo.svg";
 import cartIcon from "../assets/icons/cart.svg";
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     const cart = useSelector(
         (state) => state.cart.items
     );
@@ -14,6 +17,10 @@ function Header() {
         0
     );
 
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <header className={styles.header}>
             <div className={styles.header__container}>
@@ -21,6 +28,7 @@ function Header() {
                 <Link
                     to="/"
                     className={styles.header__logo}
+                    onClick={closeMenu}
                 >
                     <img
                         src={logo}
@@ -29,11 +37,17 @@ function Header() {
                     />
                 </Link>
 
-                <nav className={styles.header__nav}>
+                <nav
+                    className={`${styles.header__nav} ${isMenuOpen
+                        ? styles.header__nav_open
+                        : ""
+                        }`}
+                >
                     <NavLink
                         to="/"
                         className={styles.header__link}
                         end
+                        onClick={closeMenu}
                     >
                         Main Page
                     </NavLink>
@@ -41,6 +55,7 @@ function Header() {
                     <NavLink
                         to="/categories"
                         className={styles.header__link}
+                        onClick={closeMenu}
                     >
                         Categories
                     </NavLink>
@@ -48,6 +63,7 @@ function Header() {
                     <NavLink
                         to="/products"
                         className={styles.header__link}
+                        onClick={closeMenu}
                     >
                         All products
                     </NavLink>
@@ -55,32 +71,57 @@ function Header() {
                     <NavLink
                         to="/sales"
                         className={styles.header__link}
+                        onClick={closeMenu}
                     >
                         All sales
                     </NavLink>
                 </nav>
 
-                <Link
-                    to="/cart"
-                    className={styles.header__cart}
-                >
-                    <img
-                        src={cartIcon}
-                        alt="Cart"
-                        className={styles.header__cartIcon}
-                    />
+                <div className={styles.header__actions}>
 
-                    {cartCount > 0 && (
-                        <span
-                            className={
-                                styles.header__cartCount
-                            }
-                        >
-                            {cartCount}
-                        </span>
-                    )}
-                </Link>
+                    <button
+                        type="button"
+                        className={`${styles.header__burger} ${isMenuOpen
+                            ? styles.header__burger_open
+                            : ""
+                            }`}
+                        onClick={() =>
+                            setIsMenuOpen(!isMenuOpen)
+                        }
+                        aria-label={
+                            isMenuOpen
+                                ? "Close menu"
+                                : "Open menu"
+                        }
+                        aria-expanded={isMenuOpen}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
 
+                    <Link
+                        to="/cart"
+                        className={styles.header__cart}
+                    >
+                        <img
+                            src={cartIcon}
+                            alt="Cart"
+                            className={styles.header__cartIcon}
+                        />
+
+                        {cartCount > 0 && (
+                            <span
+                                className={
+                                    styles.header__cartCount
+                                }
+                            >
+                                {cartCount}
+                            </span>
+                        )}
+                    </Link>
+
+                </div>
             </div>
         </header>
     );
